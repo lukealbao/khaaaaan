523056b27e58f3149b2a7ce9f580dd1cbb6a5afc
Luke Albao <luke.albao@firstperformance.com>
2016-06-07 08:44:28 -0400

    Update limited-infection tests
    We were adding an average of 1 or 2 edges per user.
    By definition, doing this randomly will create a
    strongly connected graph, in which case - oops -
    you have only one clique.
    
    This commit generates sparser graphs.

--- test/helper/generate-data.js
+++ test/helper/generate-data.js
@@ -17,20 +17,17 @@ function createRandomEdge (user, app) {
   return app.addEdge(user.id, friendId);
 }
 
-function setupState (app, nUsers, avgEdgesPerUser) {
-  var maxEdges = avgEdgesPerUser * 2;
+function setupState (app, nUsers, nEdges) {
   for (var i = 0; i < nUsers; i++) {    
     app.addUser({name: 'Malkovich', version: '0.1.0'});
   }
 
+  var interval = Math.floor(nUsers / nEdges);
+
   // create edges for only half the users
-  for (i = 1; i <= nUsers; i += 2) {
+  for (i = 1; i <= nUsers; i += interval) {
     var user = app.model.Users.selectOne({id: i});
-    var edgeCount = Math.floor(Math.random() * maxEdges);
-    for (var j = 0; j < edgeCount; j++) {
-      createRandomEdge(user, app);
-    }
-    debug('Created %d edges for user %d', j, i);
+    createRandomEdge(user, app);
   }
   debug('Created %d users', i);
 }
--- test/limited-infection.test.js
+++ test/limited-infection.test.js
@@ -6,35 +6,74 @@ var TestSuite = require(__dirname + '/helper/success-rate.js');
 var suite = new TestSuite();
 
 suite.use(new Array(10), new Array(10));
-suite.test('Infects 10% of 1000 users with average 1 edges each', function (t) {
-  state.setup(app, 1000, 1);
-  var cliques = app.model.Cliques.select();
-  // console.log('\n\t-- Test --');
-  // console.log('Total number cliques: %d', cliques.length);
-  // console.log('Largest clique size: %d', cliques.sort(function (a,b) {
-  //               return a.count - b.count;
-  //             }).pop().count);
-
-  app.limitedInfection(100, '2.0');
+suite.test('Infects 1% of 5,000 users on 50 edges ± 0.1%', function (t) {
+  var users = 5000;
+  var edges = 50;
+  var targetPercentage = .01;
+  var e = .001;
+  
+  var targetUsers = Math.floor(users * targetPercentage);
+  var min = users * (targetPercentage - e);
+  var max = users * (targetPercentage + e);
+  state.setup(app, users, edges);  
+  
+  app.limitedInfection(targetUsers, '2.0');
   var betaUsers = app.model.Users.select({version: '2.0'});
-  // console.log('Infected %d users', betaUsers.length);
-  t.assert(betaUsers.length > 90 && betaUsers.length < 110);
+              
+  t.assert(betaUsers.length > min && betaUsers.length < max);
 });
 
-suite.test('Infects 10% of 1000 users with average 2 edges each', function (t) {
-  state.setup(app, 1000, 2);
-  var cliques = app.model.Cliques.select();
-  // console.log('\n\t-- Test --');
-  // console.log('Total number cliques: %d', cliques.length);
-  // console.log('Largest clique size: %d', cliques.sort(function (a,b) {
-  //               return a.count - b.count;
-  //             }).pop().count);
-
-  app.limitedInfection(100, '2.0');
+suite.test('Infects 10% of 5,000 users on 50 edges ± 1%', function (t) {
+  var users = 5000;
+  var edges = 50;
+  var targetPercentage = .1;
+  var e = .01;
+  
+  var targetUsers = Math.floor(users * targetPercentage);
+  var min = users * (targetPercentage - e);
+  var max = users * (targetPercentage + e);
+  state.setup(app, users, edges);  
+  
+  app.limitedInfection(targetUsers, '2.0');
   var betaUsers = app.model.Users.select({version: '2.0'});
-  // console.log('Infected %d users', betaUsers.length);
-  t.assert(betaUsers.length > 90 && betaUsers.length < 110);
+              
+  t.assert(betaUsers.length > min && betaUsers.length < max);
 });
+
+suite.test('Infects 10% of 5,000 users on 100 edges ± 1%', function (t) {
+  var users = 5000;
+  var edges = 100;
+  var targetPercentage = .1;
+  var e = .01;
+  
+  var targetUsers = Math.floor(users * targetPercentage);
+  var min = users * (targetPercentage - e);
+  var max = users * (targetPercentage + e);
+  state.setup(app, users, edges);
+  
+  app.limitedInfection(targetUsers, '2.0');
+  var betaUsers = app.model.Users.select({version: '2.0'});
+              
+  t.assert(betaUsers.length > min && betaUsers.length < max);
+});
+
+suite.test('Infects 50% of 5,000 users on 50 edges ± 5%', function (t) {
+  var users = 5000;
+  var edges = 50;
+  var targetPercentage = .1;
+  var e = .05;
+  
+  var targetUsers = Math.floor(users * targetPercentage);
+  var min = users * (targetPercentage - e);
+  var max = users * (targetPercentage + e);
+  state.setup(app, users, edges);
+  
+  app.limitedInfection(targetUsers, '2.0');
+  var betaUsers = app.model.Users.select({version: '2.0'});
+              
+  t.assert(betaUsers.length > min && betaUsers.length < max);
+});
+
 
 suite.teardown(function () {
   state.teardown(app);
